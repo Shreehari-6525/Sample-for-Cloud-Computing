b3d65b21fc68592493ca2fa0598b67357c10bc7f
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-03-17 13:27:43 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class fibonacci
+  {
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+      int num;
+       System.out.println("\n---------------FIBONACCI SERIES------------------")
+      System.out.print("Enter a Number: ");
+      num = sc.nextInt();
+
+      int a = 0;
+      int b = 1;
+      int next;
+
+      System.out.println("\n-------------------------------------------------")
+
+      for(int i = 0; i <= num; i++)
+        {
+          System.out.print(" " + a + " ");
+          next = a + b;
+          a = b;
+          b = next;
+          
+        }
+
+    }
+ 
+  }
