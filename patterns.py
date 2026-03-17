e6204a9d74a9ee673fe5ef6fc504f18e5d682cab
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-03-17 12:32:37 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,31 +1,5 @@
-# # star pattern 
 
-# #   * * * * * 
-# #   * * * * 
-# #   * * * 
-# #   * *
-# #   *
-
-value = 5
-i = 0
 print("\n============ STAR PATTERNS ===========\n\n")
-
-while value >= i:
-    print(" * " * value)
-    value -= 1
-
-
-# #   *
-# #   * *
-# #   * * *
-# #   * * * *
-# #   * * * * *
-value = 5
-
-while i <= value:
-    print(" * " * i)
-    i += 1
-
 
 
 #   * * * * *
@@ -57,28 +31,3 @@ while i <= value:
     print(" " * spaces,"* " * i)
     i += 1
     
-
-# #       *
-# #      * *
-# #     * * *
-# #    * * * *
-# #   * * * * *
-# #    * * * *
-# #     * * *
-# #      * *
-# #       *
-value = 5
-
-i = 1
-
-while i <= value:
-    spaces = value - i
-
-    print(" " * spaces,"* " * i)
-    i += 1
-
-i = value - 1
-while i >= 1:
-    spaces = value - i
-    print(" " * spaces,"* " * i)
-    i -= 1
