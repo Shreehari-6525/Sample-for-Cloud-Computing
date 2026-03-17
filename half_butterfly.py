f11bfccab6e68a5846a3c6c296006e4cca5dfa9c
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-03-17 13:47:50 +0530

    Create half_butterfly.py

---
+++ half_butterfly.py
@@ -0,0 +1,22 @@
+# #   * * * * * 
+# #   * * * * 
+# #   * * * 
+# #   * *
+# #   *
+# #   *
+# #   * *
+# #   * * *
+# #   * * * *
+# #   * * * * *
+
+
+value = 5
+i = 0
+while value >= i:
+    print(" * " * value)
+    value -= 1
+
+
+while i <= value:
+    print(" * " * i)
+    i += 1
