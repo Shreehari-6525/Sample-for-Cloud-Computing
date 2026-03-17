c64b9da5b2c609bf35f2da6608bdc047d16f539c
Shreehari-6525 <148474675+Shreehari-6525@users.noreply.github.com>
2026-03-17 13:46:31 +0530

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,16 @@
+
+#       *
+#      * *
+#     * * *
+#    * * * *
+#   * * * * *
+value = 5
+
+i = 1
+
+while i <= value:
+    spaces = value - i
+
+    print(" " * spaces,"* " * i)
+    i += 1
+    
